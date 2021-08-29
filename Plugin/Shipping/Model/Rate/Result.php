f51ed466f5ec51e0baf624ec38d2db0a4f3cb8e3
karliuka <karliuka.vitalii@gmail.com>
2021-08-29 12:38:04 +0300

    restore plugin

--- Plugin/Shipping/Model/Rate/Result.php
+++ Plugin/Shipping/Model/Rate/Result.php
@@ -57,22 +57,11 @@ class Result
     public function getAllFreeRates($result)
     {
         $rates = [];
-        $hasFreeShipping = 0;
-        
         foreach ($result ?: [] as $rate) {
-            if ($rate->getCarrier() == 'freeshipping') {
-                $hasFreeShipping = 1;
+            if ($rate->getPrice() < 0.0001 ) {
+                $rates[] = $rate;
             }
         }
-
-        if($hasFreeShipping) {
-            foreach ($result ?: [] as $rate) {                
-                if ($rate->getPrice() < 0.0001 ) {
-                    $rates[] = $rate;
-                }                            
-            }   
-        }    
-
         return $rates;
     }
 }
